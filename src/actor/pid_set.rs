d794a104324980d6b07cc2b28dc04c707379bd0c
Junichi Kato <j5ik2o@gmail.com>
2024-06-29 08:29:17 +0900

    separate tests.

--- src/actor/pid_set.rs
+++ src/actor/pid_set.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 
 use tokio::sync::Mutex;
 
-use crate::actor::actor::Pid;
 use crate::actor::pid::ExtendedPid;
 
 #[derive(Debug, Clone)]
@@ -115,98 +114,5 @@ impl PidSet {
   pub(crate) async fn get(&self, index: usize) -> Option<ExtendedPid> {
     let pids_mg = self.pids.lock().await;
     pids_mg.get(index).cloned()
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use crate::actor::actor_system::ActorSystem;
-
-  use super::*;
-
-  async fn new_pid(system: ActorSystem, address: &str, id: &str, request_id: u32) -> ExtendedPid {
-    let pid = Pid {
-      address: address.to_string(),
-      id: id.to_string(),
-      request_id,
-    };
-    ExtendedPid::new(pid, system)
-  }
-
-  #[tokio::test]
-  async fn test_pid_set_empty() {
-    let s = PidSet::new(&[]).await;
-    assert!(s.is_empty().await);
-  }
-
-  #[tokio::test]
-  async fn test_pid_set_clear() {
-    let system = ActorSystem::new(&[]).await;
-    let mut s = PidSet::new(&[]).await;
-    s.add(new_pid(system.clone(), "local", "p1", 0).await).await;
-    s.add(new_pid(system.clone(), "local", "p2", 0).await).await;
-    s.add(new_pid(system.clone(), "local", "p3", 0).await).await;
-    assert_eq!(3, s.len().await);
-    s.clear().await;
-    assert!(s.is_empty().await);
-    // assert_eq!(0, s.pids.len());
-  }
-
-  #[tokio::test]
-  async fn test_pid_set_add_small() {
-    let system = ActorSystem::new(&[]).await;
-    let mut s = PidSet::new(&[]).await;
-    let p1 = new_pid(system.clone(), "local", "p1", 0).await;
-    s.add(p1).await;
-    assert!(!s.is_empty().await);
-    let p1 = new_pid(system, "local", "p1", 0).await;
-    s.add(p1).await;
-    assert_eq!(1, s.len().await);
-  }
-
-  #[tokio::test]
-  async fn test_pid_set_values() {
-    let system = ActorSystem::new(&[]).await;
-    let mut s = PidSet::new(&[]).await;
-    s.add(new_pid(system.clone(), "local", "p1", 0).await).await;
-    s.add(new_pid(system.clone(), "local", "p2", 0).await).await;
-    s.add(new_pid(system.clone(), "local", "p3", 0).await).await;
-    assert!(!s.is_empty().await);
-
-    let r = s.to_vec().await;
-    assert_eq!(3, r.len());
-  }
-
-  #[tokio::test]
-  async fn test_pid_set_add_map() {
-    let system = ActorSystem::new(&[]).await;
-    let mut s = PidSet::new(&[]).await;
-    let p1 = new_pid(system.clone(), "local", "p1", 0).await;
-    s.add(p1).await;
-    assert!(!s.is_empty().await);
-    let p1 = new_pid(system, "local", "p1", 0).await;
-    s.add(p1).await;
-    assert_eq!(1, s.len().await);
-  }
-
-  #[tokio::test]
-  async fn test_pid_set_add_remove() {
-    let system = ActorSystem::new(&[]).await;
-    let mut pids = vec![];
-    for i in 0..1000 {
-      let pid = new_pid(system.clone(), "local", &format!("p{}", i), 0).await;
-      pids.push(pid);
-    }
-
-    let mut s = PidSet::new(&[]).await;
-    for pid in &pids {
-      s.add(pid.clone()).await;
-    }
-    assert_eq!(1000, s.len().await);
-
-    for pid in &pids {
-      assert!(s.remove(pid).await);
-    }
-    assert!(s.is_empty().await);
   }
 }
---
+++ src/actor/pid_set_test.rs
@@ -0,0 +1,90 @@
+use crate::actor::actor::Pid;
+use crate::actor::actor_system::ActorSystem;
+use crate::actor::pid::ExtendedPid;
+use crate::actor::pid_set::PidSet;
+
+async fn new_pid(system: ActorSystem, address: &str, id: &str, request_id: u32) -> ExtendedPid {
+  let pid = Pid {
+    address: address.to_string(),
+    id: id.to_string(),
+    request_id,
+  };
+  ExtendedPid::new(pid, system)
+}
+
+#[tokio::test]
+async fn test_pid_set_empty() {
+  let s = PidSet::new(&[]).await;
+  assert!(s.is_empty().await);
+}
+
+#[tokio::test]
+async fn test_pid_set_clear() {
+  let system = ActorSystem::new(&[]).await;
+  let mut s = PidSet::new(&[]).await;
+  s.add(new_pid(system.clone(), "local", "p1", 0).await).await;
+  s.add(new_pid(system.clone(), "local", "p2", 0).await).await;
+  s.add(new_pid(system.clone(), "local", "p3", 0).await).await;
+  assert_eq!(3, s.len().await);
+  s.clear().await;
+  assert!(s.is_empty().await);
+  // assert_eq!(0, s.pids.len());
+}
+
+#[tokio::test]
+async fn test_pid_set_add_small() {
+  let system = ActorSystem::new(&[]).await;
+  let mut s = PidSet::new(&[]).await;
+  let p1 = new_pid(system.clone(), "local", "p1", 0).await;
+  s.add(p1).await;
+  assert!(!s.is_empty().await);
+  let p1 = new_pid(system, "local", "p1", 0).await;
+  s.add(p1).await;
+  assert_eq!(1, s.len().await);
+}
+
+#[tokio::test]
+async fn test_pid_set_values() {
+  let system = ActorSystem::new(&[]).await;
+  let mut s = PidSet::new(&[]).await;
+  s.add(new_pid(system.clone(), "local", "p1", 0).await).await;
+  s.add(new_pid(system.clone(), "local", "p2", 0).await).await;
+  s.add(new_pid(system.clone(), "local", "p3", 0).await).await;
+  assert!(!s.is_empty().await);
+
+  let r = s.to_vec().await;
+  assert_eq!(3, r.len());
+}
+
+#[tokio::test]
+async fn test_pid_set_add_map() {
+  let system = ActorSystem::new(&[]).await;
+  let mut s = PidSet::new(&[]).await;
+  let p1 = new_pid(system.clone(), "local", "p1", 0).await;
+  s.add(p1).await;
+  assert!(!s.is_empty().await);
+  let p1 = new_pid(system, "local", "p1", 0).await;
+  s.add(p1).await;
+  assert_eq!(1, s.len().await);
+}
+
+#[tokio::test]
+async fn test_pid_set_add_remove() {
+  let system = ActorSystem::new(&[]).await;
+  let mut pids = vec![];
+  for i in 0..1000 {
+    let pid = new_pid(system.clone(), "local", &format!("p{}", i), 0).await;
+    pids.push(pid);
+  }
+
+  let mut s = PidSet::new(&[]).await;
+  for pid in &pids {
+    s.add(pid.clone()).await;
+  }
+  assert_eq!(1000, s.len().await);
+
+  for pid in &pids {
+    assert!(s.remove(pid).await);
+  }
+  assert!(s.is_empty().await);
+}
