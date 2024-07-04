5116ebc8a17b38622e7866f5b6fc74601c09700a
Junichi Kato <j5ik2o@gmail.com>
2024-07-04 12:12:19 +0900

    improve test.

--- src/actor/spawn_named_example_test.rs
+++ src/actor/spawn_named_example_test.rs
@@ -1,4 +1,3 @@
-use crate::actor::actor::props::Props;
 use crate::actor::actor::receive_func::ReceiveFunc;
 use crate::actor::actor_system::ActorSystem;
 use crate::actor::context::{MessagePart, SpawnerPart};
@@ -7,9 +6,10 @@ use crate::actor::message::system_message::SystemMessage;
 use crate::actor::util::async_barrier::AsyncBarrier;
 use std::env;
 use tracing_subscriber::EnvFilter;
+use crate::actor::actor::props::Props;
 
 #[tokio::test]
-async fn example_root_context_spawn() {
+async fn example_root_context_spawn_named() {
   let _ = env::set_var("RUST_LOG", "debug");
   let _ = tracing_subscriber::fmt()
     .with_env_filter(EnvFilter::from_default_env())
@@ -20,20 +20,22 @@ async fn example_root_context_spawn() {
   let cloned_b = b.clone();
 
   let props = Props::from_receive_func(ReceiveFunc::new(move |ctx| {
-    let b = cloned_b.clone();
+    let cloned_b = cloned_b.clone();
     async move {
       let msg = ctx.get_message().await.unwrap();
       if let Some(SystemMessage::Started(sm)) = msg.as_any().downcast_ref::<SystemMessage>() {
         println!("Hello World!");
-        b.wait().await;
+        cloned_b.wait().await;
       }
       Ok(())
     }
   }))
   .await;
 
-  let pid = system.get_root_context().await.spawn(props).await;
-  tracing::debug!("pid = {:?}", pid);
+  let result = system.get_root_context().await.spawn_named(props, "my-actor").await;
+  if let Err(err) = result {
+    panic!("Failed to spawn actor: {:?}", err);
+  }
 
   b.wait().await;
 }
