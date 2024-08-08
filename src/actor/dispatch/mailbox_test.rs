679ec85f1485cf40833b9a999fedb2c19823f09a
Junichi Kato <j5ik2o@gmail.com>
2024-08-08 17:50:01 +0900

    test: Improve unbounded mailbox system message consistency

--- src/actor/dispatch/mailbox_test.rs
+++ src/actor/dispatch/mailbox_test.rs
@@ -121,61 +121,61 @@ mod tests {
     }
   }
 
-    #[tokio::test]
-    async fn test_unbounded_mpsc_mailbox_system_message_consistency() {
-        let _ = env::set_var("RUST_LOG", "debug");
-        let _ = tracing_subscriber::fmt()
-            .with_env_filter(EnvFilter::from_default_env())
-            .try_init();
+  #[tokio::test]
+  async fn test_unbounded_mpsc_mailbox_system_message_consistency() {
+    let _ = env::set_var("RUST_LOG", "debug");
+    let _ = tracing_subscriber::fmt()
+      .with_env_filter(EnvFilter::from_default_env())
+      .try_init();
 
-        let max = 100;
-        let c = 10;
+    let max = 100;
+    let c = 10;
 
-        let mbox_producer = unbounded_mpsc_mailbox_creator();
-        let message_invoker = Arc::new(Mutex::new(TestMessageInvoker::new(max)));
-        let mut mailbox = mbox_producer.run().await;
+    let mbox_producer = unbounded_mpsc_mailbox_creator();
+    let message_invoker = Arc::new(Mutex::new(TestMessageInvoker::new(max)));
+    let mut mailbox = mbox_producer.run().await;
 
-        let dispatcher = TokioRuntimeContextDispatcher::new().unwrap();
+    let dispatcher = TokioRuntimeContextDispatcher::new().unwrap();
 
-        mailbox
-            .register_handlers(
-                Some(MessageInvokerHandle::new(message_invoker.clone())),
-                Some(DispatcherHandle::new(dispatcher.clone())),
-            )
+    mailbox
+      .register_handlers(
+        Some(MessageInvokerHandle::new(message_invoker.clone())),
+        Some(DispatcherHandle::new(dispatcher.clone())),
+      )
+      .await;
+
+    let mut join_handles = Vec::new();
+    let rng = SmallRng::from_thread_rng();
+
+    for j in 0..c {
+      let cmax = max / c;
+      let mailbox = mailbox.clone();
+      let mut rng = rng.clone();
+
+      let h = tokio::spawn(async move {
+        for i in 0..cmax {
+          if rng.gen_range(0..10) == 0 {
+            let wait_time = rng.gen_range(0..1000);
+            sleep(Duration::from_millis(wait_time)).await;
+          }
+          mailbox
+            .post_system_message(MessageHandle::new(format!("{} {}", j, i)))
             .await;
-
-        let mut join_handles = Vec::new();
-        let rng = SmallRng::from_thread_rng();
-
-        for j in 0..c {
-            let cmax = max / c;
-            let mailbox = mailbox.clone();
-            let mut rng = rng.clone();
-
-            let h = tokio::spawn(async move {
-                for i in 0..cmax {
-                    if rng.gen_range(0..10) == 0 {
-                        let wait_time = rng.gen_range(0..1000);
-                        sleep(Duration::from_millis(wait_time)).await;
-                    }
-                    mailbox
-                        .post_system_message(MessageHandle::new(format!("{} {}", j, i)))
-                        .await;
-                }
-            });
-            join_handles.push(h);
         }
-
-        for h in join_handles {
-            h.await.unwrap();
-        }
-
-        sleep(Duration::from_secs(1)).await;
-
-        {
-            let mg = message_invoker.lock().await;
-            assert_eq!(mg.get_count(), max);
-            assert!(mg.is_assert_flg());
-        }
+      });
+      join_handles.push(h);
     }
+
+    for h in join_handles {
+      h.await.unwrap();
+    }
+
+    sleep(Duration::from_secs(1)).await;
+
+    {
+      let mg = message_invoker.lock().await;
+      assert_eq!(mg.get_count(), max);
+      assert!(mg.is_assert_flg());
+    }
+  }
 }
