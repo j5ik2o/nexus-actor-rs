5b7ceb2f2374b29748aa77d403894ff705db0f5e
Junichi Kato <j5ik2o@gmail.com>
2024-10-08 06:52:24 +0900

    fix: remove files

--- core/src/actor/message/terminate_info.rs
+++
@@ -1,8 +0,0 @@
-use crate::actor::message::terminate_reason::TerminateReason;
-use crate::generated::actor::Pid;
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct TerminateInfo {
-  pub who: Option<Pid>,
-  pub why: TerminateReason,
-}
