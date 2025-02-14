79735ecbc869e27a84857f25a033901391f91746
Junichi Kato <j5ik2o@gmail.com>
2025-02-14 23:10:19 +0000

    feat(core): add event stream implementation with cluster and remote support

--- core/src/event_stream/mod.rs
+++ core/src/event_stream/mod.rs
@@ -1,9 +1,7 @@
-mod event;
-mod stream;
+mod event_handler;
+mod event_stream_impl;
+mod event_stream_test;
+mod predicate;
 mod subscription;
 
-pub use self::{
-    event::*,
-    stream::*,
-    subscription::*,
-};
+pub use {self::event_handler::*, self::event_stream_impl::*, self::predicate::*, self::subscription::*};
