441d833973cdd55ac9091debc979ad3223e4aa8f
Junichi Kato <j5ik2o@gmail.com>
2025-02-14 23:59:16 +0000

    refactor: fix TypedMessageOrEnvelope implementation

--- core/src/actor/message/typed_message_or_envelope.rs
+++ core/src/actor/message/typed_message_or_envelope.rs
@@ -1,80 +1,67 @@
-use crate::actor::actor::ExtendedPid;
-use crate::actor::message::message_headers::MessageHeaders;
-use crate::actor::message::message_or_envelope::MessageEnvelope;
 use crate::actor::message::Message;
+use crate::actor::message::message_handle::MessageHandle;
+use crate::actor::message::message_headers::MessageHeaders;
+use crate::actor::pid::Pid;
 use std::any::Any;
+use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
 pub struct TypedMessageOrEnvelope<T: Message> {
-  underlying: MessageEnvelope,
-  _phantom: std::marker::PhantomData<T>,
+    pub(crate) message: T,
+    pub(crate) header: Option<MessageHeaders>,
+    pub(crate) sender: Option<Pid>,
 }
 
-impl<M: Message> PartialEq for TypedMessageOrEnvelope<M> {
-  fn eq(&self, other: &Self) -> bool {
-    self.underlying == other.underlying
-  }
-}
-
-impl<M: Message> Message for TypedMessageOrEnvelope<M> {
-  fn eq_message(&self, other: &dyn Message) -> bool {
-    if let Some(other) = other.as_any().downcast_ref::<Self>() {
-      self == other
-    } else {
-      false
+impl<T: Message> Message for TypedMessageOrEnvelope<T> {
+    fn eq_message(&self, other: &dyn Message) -> bool {
+        if let Some(other) = other.as_any().downcast_ref::<TypedMessageOrEnvelope<T>>() {
+            self.message.eq_message(&other.message)
+        } else {
+            false
+        }
     }
-  }
 
-  fn as_any(&self) -> &dyn Any {
-    self
-  }
-
-  fn message_type(&self) -> &'static str {
-    "TypedMessageOrEnvelope"
-  }
-}
-
-impl<M: Message + Clone> TypedMessageOrEnvelope<M> {
-  pub fn new(underlying: MessageEnvelope) -> Self {
-    Self {
-      underlying,
-      _phantom: std::marker::PhantomData,
+    fn as_any(&self) -> &dyn Any {
+        self
     }
-  }
 
-  pub fn get_message(&self) -> M {
-    self.underlying.get_message_handle().to_typed::<M>().unwrap()
-  }
-
-  pub fn get_sender(&self) -> Option<ExtendedPid> {
-    self.underlying.get_sender().clone()
-  }
-
-  pub fn get_header(&self) -> Option<MessageHeaders> {
-    self.underlying.get_header().clone()
-  }
-
-  pub fn get_header_value(&self, key: &str) -> Option<String> {
-    self.underlying.get_header_value(key)
-  }
-
-  pub fn set_header(&mut self, key: String, value: String) {
-    self.underlying.set_header(key, value);
-  }
-
-  pub fn get_headers(&self) -> Option<MessageHeaders> {
-    self.underlying.get_headers()
-  }
+    fn message_type(&self) -> &'static str {
+        "TypedMessageOrEnvelope"
+    }
 }
 
-impl<M: Message + Clone> From<MessageEnvelope> for TypedMessageOrEnvelope<M> {
-  fn from(underlying: MessageEnvelope) -> Self {
-    TypedMessageOrEnvelope::new(underlying)
-  }
-}
+impl<T: Message> TypedMessageOrEnvelope<T> {
+    pub fn new(message: T) -> Self {
+        Self {
+            message,
+            header: None,
+            sender: None,
+        }
+    }
 
-impl<M: Message> From<TypedMessageOrEnvelope<M>> for MessageEnvelope {
-  fn from(typed: TypedMessageOrEnvelope<M>) -> Self {
-    typed.underlying
-  }
+    pub fn with_header(mut self, header: MessageHeaders) -> Self {
+        self.header = Some(header);
+        self
+    }
+
+    pub fn with_sender(mut self, sender: Pid) -> Self {
+        self.sender = Some(sender);
+        self
+    }
+
+    pub fn get_message(&self) -> &T {
+        &self.message
+    }
+
+    pub fn into_message(self) -> T {
+        self.message
+    }
+
+    pub fn get_sender(&self) -> Option<&Pid> {
+        self.sender.as_ref()
+    }
+
+    pub fn get_header(&self) -> Option<&MessageHeaders> {
+        self.header.as_ref()
+    }
 }
