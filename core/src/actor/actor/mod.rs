e6c2279cd6e4906b2e9015420d9d65aaf59ad59b
Junichi Kato <j5ik2o@gmail.com>
2025-02-14 23:56:04 +0000

    refactor: add process module

--- core/src/actor/actor/mod.rs
+++ core/src/actor/actor/mod.rs
@@ -2,8 +2,10 @@ pub mod actor;
 pub mod actor_error;
 pub mod actor_handle;
 pub mod actor_ref;
+pub mod error_reason;
 pub mod lifecycle;
 pub mod pid;
+pub mod process;
 pub mod props;
 pub mod restart_statistics;
 pub mod spawner;
@@ -14,8 +16,10 @@ pub use self::{
     actor_error::*,
     actor_handle::*,
     actor_ref::*,
+    error_reason::*,
     lifecycle::*,
     pid::*,
+    process::*,
     props::*,
     restart_statistics::*,
     spawner::*,
