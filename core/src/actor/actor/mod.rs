7c5149b1fd02bd85f9063937a2fb7ff6b34a6f38
Junichi Kato <j5ik2o@gmail.com>
2025-02-14 23:48:06 +0000

    refactor: update actor module structure

--- core/src/actor/actor/mod.rs
+++ core/src/actor/actor/mod.rs
@@ -3,5 +3,19 @@ pub mod actor_error;
 pub mod actor_handle;
 pub mod actor_ref;
 pub mod lifecycle;
+pub mod props;
+pub mod restart_statistics;
+pub mod spawner;
+pub mod typed_actor;
 
-pub use self::{actor::*, actor_error::*, actor_handle::*, actor_ref::*, lifecycle::*};
+pub use self::{
+    actor::*,
+    actor_error::*,
+    actor_handle::*,
+    actor_ref::*,
+    lifecycle::*,
+    props::*,
+    restart_statistics::*,
+    spawner::*,
+    typed_actor::*,
+};
