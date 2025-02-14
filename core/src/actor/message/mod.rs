44001c0666ece4f8c6a0f967919924c013f0183f
Junichi Kato <j5ik2o@gmail.com>
2025-02-14 23:49:05 +0000

    refactor: remove duplicate message.rs file

---
+++ core/src/actor/message/mod.rs
@@ -0,0 +1,37 @@
+pub mod auto_receive_message;
+pub mod auto_respond;
+pub mod continuation;
+pub mod dead_letter_response;
+pub mod failure;
+pub mod ignore_dead_letter_logging;
+pub mod message_batch;
+pub mod message_handle;
+pub mod message_headers;
+pub mod message_or_envelope;
+pub mod readonly_message_headers;
+pub mod receive_timeout;
+pub mod response;
+pub mod serialization;
+pub mod system_message;
+pub mod touched;
+pub mod typed_message_or_envelope;
+
+pub use self::{
+    auto_receive_message::*,
+    auto_respond::*,
+    continuation::*,
+    dead_letter_response::*,
+    failure::*,
+    ignore_dead_letter_logging::*,
+    message_batch::*,
+    message_handle::*,
+    message_headers::*,
+    message_or_envelope::*,
+    readonly_message_headers::*,
+    receive_timeout::*,
+    response::*,
+    serialization::*,
+    system_message::*,
+    touched::*,
+    typed_message_or_envelope::*,
+};
