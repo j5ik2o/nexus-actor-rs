4fdd4ef33d437620b435d4dec95462aedbfefe09
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 01:28:47 +0000

    fix: update root context implementation

--- core/src/actor/context/root_context.rs
+++ core/src/actor/context/root_context.rs
@@ -6,51 +6,59 @@ use std::time::Duration;
 use tokio::sync::RwLock;
 
 use crate::actor::{
-  ActorContext, ActorError, ActorHandle, ActorSystem, Message, MessageHandle, MessageOrEnvelope, Pid, Props,
-  ReadonlyMessageHeadersHandle, ResponseHandle, SpawnError, TypedRootContext,
+  ActorError, ActorSystem, Context, InfoPart, Message, MessageHandle, MessageOrEnvelope, MessagePart, Pid, Props,
+  ReceiverPart, SenderPart, SpawnError, SpawnerPart, StopperPart,
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct RootContext {
-  actor_system: ActorSystem,
-  guardian_strategy: Option<Arc<RwLock<dyn ActorContext>>>,
+  pub(crate) actor_system: Arc<RwLock<ActorSystem>>,
 }
 
+impl RootContext {
+  pub fn new(actor_system: Arc<RwLock<ActorSystem>>) -> Self {
+    Self { actor_system }
+  }
+}
+
+#[async_trait]
 impl Context for RootContext {
   fn as_any(&self) -> &dyn Any {
     self
+  }
+
+  async fn parent(&self) -> Option<Pid> {
+    None
+  }
+
+  async fn self_pid(&self) -> Pid {
+    unimplemented!()
+  }
+
+  async fn actor_system(&self) -> Arc<RwLock<ActorSystem>> {
+    self.actor_system.clone()
   }
 }
 
 #[async_trait]
 impl InfoPart for RootContext {
-  async fn get_children(&self) -> Vec<Pid> {
-    if let Some(g) = &self.guardian_strategy {
-      g.read().await.get_children().await
-    } else {
-      Vec::new()
-    }
-  }
-
-  async fn get_receive_timeout(&self) -> Duration {
-    Duration::from_secs(0)
-  }
-
-  async fn get_parent(&self) -> Option<Pid> {
-    None
-  }
-
   async fn get_self_opt(&self) -> Option<Pid> {
     None
   }
 
-  async fn set_self(&mut self, _pid: Pid) {}
+  async fn get_self(&self) -> Pid {
+    unimplemented!()
+  }
 
-  async fn get_actor(&self) -> Option<ActorHandle> {
+  async fn get_parent_opt(&self) -> Option<Pid> {
     None
   }
 
-  async fn get_actor_system(&self) -> ActorSystem {
+  async fn get_parent(&self) -> Pid {
+    unimplemented!()
+  }
+
+  async fn get_actor_system(&self) -> Arc<RwLock<ActorSystem>> {
     self.actor_system.clone()
   }
 }
@@ -58,85 +66,71 @@ impl InfoPart for RootContext {
 #[async_trait]
 impl MessagePart for RootContext {
   async fn get_message(&self) -> MessageHandle {
-    unimplemented!("Root context does not handle messages")
-  }
-
-  async fn get_message_header_handle(&self) -> Option<ReadonlyMessageHeadersHandle> {
-    None
+    unimplemented!()
   }
 
   async fn get_message_envelope_opt(&self) -> Option<MessageOrEnvelope> {
     None
   }
 
-  async fn get_message_handle_opt(&self) -> Option<MessageHandle> {
-    None
+  async fn get_message_envelope(&self) -> MessageOrEnvelope {
+    unimplemented!()
   }
+
+  async fn get_receive_timeout(&self) -> Duration {
+    Duration::from_secs(0)
+  }
+
+  async fn set_receive_timeout(&self, _duration: Duration) {}
+
+  async fn cancel_receive_timeout(&self) {}
 }
 
 #[async_trait]
 impl SenderPart for RootContext {
-  async fn forward(&self, _pid: &Pid) {}
-
-  async fn respond(&self, _response: ResponseHandle) {}
-
-  async fn get_sender(&self) -> Option<Pid> {
-    None
+  async fn send(&self, target: &Pid, message: MessageHandle) {
+    self.actor_system.read().await.send(target, message).await;
   }
 
-  async fn send(&mut self, pid: Pid, message_handle: MessageHandle) {
-    self.actor_system.send_user_message(pid, message_handle).await;
+  async fn request(&self, target: &Pid, message: MessageHandle) -> MessageHandle {
+    self.actor_system.read().await.request(target, message).await
   }
 
-  async fn request(&mut self, pid: Pid, message_handle: MessageHandle) {
-    self.send(pid, message_handle).await;
-  }
-
-  async fn request_with_custom_sender(&mut self, pid: Pid, message_handle: MessageHandle, _sender: Pid) {
-    self.send(pid, message_handle).await;
-  }
-
-  async fn request_future(&self, pid: Pid, message_handle: MessageHandle) -> Result<ResponseHandle, ActorError> {
-    self.actor_system.request_future(pid, message_handle).await
+  async fn forward(&self, target: &Pid, message: MessageHandle) {
+    self.actor_system.read().await.forward(target, message).await;
   }
 }
 
 #[async_trait]
 impl SpawnerPart for RootContext {
   async fn spawn(&self, props: Props) -> Result<Pid, SpawnError> {
-    self.actor_system.spawn(props).await
+    self.actor_system.read().await.spawn(props).await
   }
 
   async fn spawn_prefix(&self, props: Props, prefix: &str) -> Result<Pid, SpawnError> {
-    self.actor_system.spawn_prefix(props, prefix).await
-  }
-
-  async fn spawn_named(&mut self, props: Props, id: &str) -> Result<Pid, SpawnError> {
-    self.actor_system.spawn_named(props, id).await
+    self.actor_system.read().await.spawn_prefix(props, prefix).await
   }
 }
 
 #[async_trait]
 impl StopperPart for RootContext {
-  async fn stop(&self) {}
-
-  async fn poison_pill(&self) {}
-}
-
-impl RootContext {
-  pub fn new(actor_system: ActorSystem) -> Self {
-    Self {
-      actor_system,
-      guardian_strategy: None,
-    }
+  async fn stop(&self, pid: &Pid) {
+    self.actor_system.read().await.stop(pid).await;
   }
 
-  pub fn with_guardian(mut self, guardian: Arc<RwLock<dyn ActorContext>>) -> Self {
-    self.guardian_strategy = Some(guardian);
-    self
+  async fn poison_pill(&self, pid: &Pid) {
+    self.actor_system.read().await.poison_pill(pid).await;
   }
 
-  pub fn to_typed<M: Message>(&self) -> TypedRootContext<M> {
-    TypedRootContext::new(self.clone())
+  async fn watch(&self, pid: &Pid) {
+    self.actor_system.read().await.watch(pid).await;
+  }
+
+  async fn unwatch(&self, pid: &Pid) {
+    self.actor_system.read().await.unwatch(pid).await;
+  }
+
+  async fn handle_failure(&self, who: Option<Pid>, error: ActorError, message: Option<MessageHandle>) {
+    self.actor_system.read().await.handle_failure(who, error, message).await;
   }
 }
