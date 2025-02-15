24ba26044e00829129809d1fbd3aeed878bed7e1
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 00:06:17 +0000

    refactor: fix Process trait and add helper functions

--- core/src/actor/process.rs
+++ core/src/actor/process.rs
@@ -1,15 +1,22 @@
+use std::fmt::Debug;
+use async_trait::async_trait;
 use crate::actor::message::Message;
 use crate::actor::pid::Pid;
-use async_trait::async_trait;
-use std::fmt::Debug;
 
 #[async_trait]
 pub trait Process: Debug + Send + Sync + 'static {
-  async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>);
-  async fn send_system_message(&self, message: Box<dyn Message>);
-  async fn stop(&self);
-  async fn set_dead(&self);
+    async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>);
+    async fn send_system_message(&self, message: Box<dyn Message>);
+    async fn stop(&self);
+    async fn set_dead(&self);
 }
 
 pub type ActorProcess = Box<dyn Process + Send + Sync>;
 pub type ProcessHandle = Box<dyn Process + Send + Sync>;
+
+// Helper functions for creating process handles
+impl ProcessHandle {
+    pub fn new(process: Box<dyn Process + Send + Sync>) -> Self {
+        process
+    }
+}
