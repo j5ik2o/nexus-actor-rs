30f24f380d745114db94e68682bcc031e5827124
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 01:51:59 +0000

    fix: update actor and message envelope implementations

--- core/src/actor/actor/actor.rs
+++ core/src/actor/actor/actor.rs
@@ -1,10 +1,26 @@
-use crate::actor::{Context, MessageOrEnvelope};
+//! Actor trait implementation.
+
 use async_trait::async_trait;
 use std::fmt::Debug;
 
+use crate::actor::actor_error::ActorError;
+use crate::actor::context::Context;
+use crate::actor::message::MessageOrEnvelope;
+use crate::actor::supervisor::SupervisorStrategy;
+
 #[async_trait]
 pub trait Actor: Debug + Send + Sync + 'static {
-  async fn started(&mut self, ctx: &dyn Context) {}
-  async fn stopped(&mut self, ctx: &dyn Context) {}
   async fn receive(&mut self, ctx: &dyn Context, msg: MessageOrEnvelope);
+  async fn started(&mut self, _ctx: &dyn Context) -> Result<(), ActorError> {
+    Ok(())
+  }
+  async fn stopped(&mut self, _ctx: &dyn Context) -> Result<(), ActorError> {
+    Ok(())
+  }
+  async fn restarting(&mut self, _ctx: &dyn Context) -> Result<(), ActorError> {
+    Ok(())
+  }
+  async fn get_supervisor_strategy(&mut self) -> Option<SupervisorStrategy> {
+    None
+  }
 }
--- core/src/actor/message/message_or_envelope.rs
+++ core/src/actor/message/message_or_envelope.rs
@@ -1,47 +1,58 @@
+//! Message or envelope implementation.
+
+use std::any::Any;
 use std::fmt::Debug;
 
-use crate::actor::message::{message_headers::MessageHeaders, Message};
+use crate::actor::message::{Message, MessageHeaders};
 use crate::actor::pid::Pid;
 
 #[derive(Debug, Clone)]
-pub struct MessageOrEnvelope {
-  pub(crate) message: Box<dyn Message>,
-  pub(crate) header: Option<MessageHeaders>,
-  pub(crate) sender: Option<Pid>,
+pub enum MessageOrEnvelope {
+  Message(Box<dyn Message>),
+  Envelope {
+    message: Box<dyn Message>,
+    header: MessageHeaders,
+    sender: Option<Pid>,
+  },
+}
+
+impl Message for MessageOrEnvelope {
+  fn as_any(&self) -> &dyn Any {
+    self
+  }
 }
 
 impl MessageOrEnvelope {
-  pub fn new(message: Box<dyn Message>) -> Self {
-    Self {
+  pub fn new_message(message: Box<dyn Message>) -> Self {
+    Self::Message(message)
+  }
+
+  pub fn new_envelope(message: Box<dyn Message>, header: MessageHeaders, sender: Option<Pid>) -> Self {
+    Self::Envelope {
       message,
-      header: None,
-      sender: None,
+      header,
+      sender,
     }
   }
 
-  pub fn with_header(mut self, header: MessageHeaders) -> Self {
-    self.header = Some(header);
-    self
-  }
-
-  pub fn with_sender(mut self, sender: Pid) -> Self {
-    self.sender = Some(sender);
-    self
-  }
-
-  pub fn get_message(&self) -> &dyn Message {
-    &*self.message
-  }
-
-  pub fn into_message(self) -> Box<dyn Message> {
-    self.message
-  }
-
-  pub fn get_sender(&self) -> Option<&Pid> {
-    self.sender.as_ref()
+  pub fn get_message(&self) -> &Box<dyn Message> {
+    match self {
+      Self::Message(msg) => msg,
+      Self::Envelope { message, .. } => message,
+    }
   }
 
   pub fn get_header(&self) -> Option<&MessageHeaders> {
-    self.header.as_ref()
+    match self {
+      Self::Message(_) => None,
+      Self::Envelope { header, .. } => Some(header),
+    }
+  }
+
+  pub fn get_sender(&self) -> Option<&Pid> {
+    match self {
+      Self::Message(_) => None,
+      Self::Envelope { sender, .. } => sender.as_ref(),
+    }
   }
 }
