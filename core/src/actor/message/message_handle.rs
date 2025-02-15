5ebcf5243d241a785efd0afbae363c7f5c515476
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 01:52:40 +0000

    fix: update message handle and process trait implementations

--- core/src/actor/message/message_handle.rs
+++ core/src/actor/message/message_handle.rs
@@ -1,26 +1,23 @@
 //! Message handle implementation.
 
-use std::any::Any;
-use std::fmt::Debug;
-use std::sync::Arc;
-use tokio::sync::RwLock;
-
 use crate::actor::message::Message;
+use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
 pub struct MessageHandle {
-  inner: Arc<RwLock<Box<dyn Message>>>,
+  message: Box<dyn Message>,
 }
 
 impl MessageHandle {
   pub fn new(message: Box<dyn Message>) -> Self {
-    Self {
-      inner: Arc::new(RwLock::new(message)),
-    }
+    Self { message }
   }
 
-  pub async fn to_typed<T: Message>(&self) -> Option<T> {
-    let message = self.inner.read().await;
-    message.as_any().downcast_ref::<T>().cloned()
+  pub fn get_message(&self) -> &Box<dyn Message> {
+    &self.message
+  }
+
+  pub async fn to_typed<T: Message + Clone>(&self) -> Option<T> {
+    self.message.as_any().downcast_ref::<T>().cloned()
   }
 }
