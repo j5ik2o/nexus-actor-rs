cf448da25a5333ed9075970092d47446825614d6
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 00:00:48 +0000

    refactor: simplify Process trait

--- core/src/actor/process.rs
+++ core/src/actor/process.rs
@@ -11,59 +11,6 @@ pub trait Process: Debug + Send + Sync + 'static {
     async fn set_dead(&self);
 }
 
-#[async_trait]
-impl Process for crate::actor::dispatch::future::ActorFutureProcess {
-    async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>) {
-        // Implementation
-    }
+pub type ActorProcess = Box<dyn Process + Send + Sync>;
 
-    async fn send_system_message(&self, message: Box<dyn Message>) {
-        // Implementation
-    }
-
-    async fn stop(&self) {
-        // Implementation
-    }
-
-    async fn set_dead(&self) {
-        // Implementation
-    }
-}
-
-#[async_trait]
-impl Process for crate::actor::event_stream::event_stream_process::EventStreamProcess {
-    async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>) {
-        // Implementation
-    }
-
-    async fn send_system_message(&self, message: Box<dyn Message>) {
-        // Implementation
-    }
-
-    async fn stop(&self) {
-        // Implementation
-    }
-
-    async fn set_dead(&self) {
-        // Implementation
-    }
-}
-
-#[async_trait]
-impl Process for crate::actor::guardian::GuardianProcess {
-    async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>) {
-        // Implementation
-    }
-
-    async fn send_system_message(&self, message: Box<dyn Message>) {
-        // Implementation
-    }
-
-    async fn stop(&self) {
-        // Implementation
-    }
-
-    async fn set_dead(&self) {
-        // Implementation
-    }
-}
+// Remove implementations to avoid conflicts - these should be in their respective modules
