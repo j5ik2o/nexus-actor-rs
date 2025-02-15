841644d569659ceeafceb96f64a195b5b13447a3
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 01:06:06 +0000

    fix: update UnboundedQueue and Message trait implementations

--- core/src/actor/dispatch/unbounded.rs
+++ core/src/actor/dispatch/unbounded.rs
@@ -1,9 +1,10 @@
+use async_trait::async_trait;
 use std::fmt::Debug;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
 use crate::actor::message::MessageHandle;
-use nexus_actor_utils_rs::collections::{Element, Queue, QueueBase, QueueReader, QueueWriter};
+use nexus_actor_utils_rs::collections::{Element, Queue, QueueBase, QueueError, QueueReader, QueueSize, QueueWriter};
 
 impl Element for MessageHandle {
   fn clone_element(&self) -> Self {
@@ -16,16 +17,22 @@ pub struct UnboundedQueue<Q: QueueReader<MessageHandle> + QueueWriter<MessageHan
   queue: Arc<RwLock<Q>>,
 }
 
+#[async_trait]
 impl<Q: QueueReader<MessageHandle> + QueueWriter<MessageHandle>> QueueBase<MessageHandle> for UnboundedQueue<Q> {
-  fn len(&self) -> usize {
-    self.queue.read().await.len()
+  async fn len(&self) -> usize {
+    self.queue.read().await.len().await
   }
 
-  fn is_empty(&self) -> bool {
-    self.queue.read().await.is_empty()
+  async fn is_empty(&self) -> bool {
+    self.queue.read().await.is_empty().await
+  }
+
+  async fn capacity(&self) -> QueueSize {
+    self.queue.read().await.capacity().await
   }
 }
 
+#[async_trait]
 impl<Q: QueueReader<MessageHandle> + QueueWriter<MessageHandle>> QueueReader<MessageHandle> for UnboundedQueue<Q> {
   async fn dequeue(&mut self) -> Option<MessageHandle> {
     self.queue.write().await.dequeue().await
@@ -34,11 +41,24 @@ impl<Q: QueueReader<MessageHandle> + QueueWriter<MessageHandle>> QueueReader<Mes
   async fn peek(&self) -> Option<&MessageHandle> {
     self.queue.read().await.peek().await
   }
+
+  async fn poll(&mut self) -> Result<Option<MessageHandle>, QueueError<MessageHandle>> {
+    self.queue.write().await.poll().await
+  }
+
+  async fn clean_up(&mut self) {
+    self.queue.write().await.clean_up().await;
+  }
 }
 
+#[async_trait]
 impl<Q: QueueReader<MessageHandle> + QueueWriter<MessageHandle>> QueueWriter<MessageHandle> for UnboundedQueue<Q> {
   async fn enqueue(&mut self, item: MessageHandle) {
     self.queue.write().await.enqueue(item).await;
+  }
+
+  async fn offer(&mut self, item: MessageHandle) -> Result<(), QueueError<MessageHandle>> {
+    self.queue.write().await.offer(item).await
   }
 }
 
