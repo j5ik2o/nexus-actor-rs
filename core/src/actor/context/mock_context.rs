5c2053375f7dddf269e20297713a6a66b8376e82
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 00:53:35 +0000

    fix: update mock context and process implementations

--- core/src/actor/context/mock_context.rs
+++ core/src/actor/context/mock_context.rs
@@ -1,194 +1,133 @@
-use crate::actor::ActorError;
-use crate::actor::ActorHandle;
-use crate::actor::Continuer;
-use crate::actor::ExtendedPid;
-use crate::actor::Props;
-use crate::actor::SpawnError;
-use crate::actor::actor_system::ActorSystem;
-use crate::actor::context::{
-  BasePart, Context, ExtensionContext, ExtensionPart, InfoPart, MessagePart, ReceiverContext, ReceiverPart,
-  SenderContext, SenderPart, SpawnerContext, SpawnerPart, StopperPart,
-};
-use crate::actor::dispatch::future::{ActorFuture, ActorFutureProcess};
-use crate::actor::message::MessageEnvelope;
-use crate::actor::message::MessageHandle;
-use crate::actor::message::ReadonlyMessageHeadersHandle;
-use crate::actor::message::ResponseHandle;
-use crate::actor::process::Process;
-use crate::ctxext::extensions::{ContextExtensionHandle, ContextExtensionId};
 use async_trait::async_trait;
 use std::any::Any;
+use std::fmt::Debug;
+use std::sync::Arc;
 use std::time::Duration;
+use tokio::sync::RwLock;
 
-#[derive(Debug, Clone)]
+use crate::actor::{
+  ActorContext, ActorError, ActorHandle, ActorSystem, Message, MessageHandle, MessageOrEnvelope, Pid, Props,
+  ReadonlyMessageHeadersHandle, ResponseHandle, SpawnError,
+};
+
+#[derive(Debug)]
 pub struct MockContext {
-  system: ActorSystem,
+  actor_system: ActorSystem,
+  extensions: Arc<RwLock<Vec<Box<dyn Any + Send + Sync>>>>,
 }
 
-impl MockContext {
-  pub fn new(system: ActorSystem) -> Self {
-    Self { system }
+impl Context for MockContext {
+  fn as_any(&self) -> &dyn Any {
+    self
   }
 }
-
-impl ExtensionContext for MockContext {}
-
-#[async_trait]
-impl ExtensionPart for MockContext {
-  async fn get(&mut self, _: ContextExtensionId) -> Option<ContextExtensionHandle> {
-    None
-  }
-
-  async fn set(&mut self, _: ContextExtensionHandle) {}
-}
-
-impl SenderContext for MockContext {}
 
 #[async_trait]
 impl InfoPart for MockContext {
-  async fn get_parent(&self) -> Option<ExtendedPid> {
+  async fn get_children(&self) -> Vec<Pid> {
+    Vec::new()
+  }
+
+  async fn get_receive_timeout(&self) -> Duration {
+    Duration::from_secs(0)
+  }
+
+  async fn get_parent(&self) -> Option<Pid> {
     None
   }
 
-  async fn get_self_opt(&self) -> Option<ExtendedPid> {
+  async fn get_self_opt(&self) -> Option<Pid> {
     None
   }
 
-  async fn set_self(&mut self, _: ExtendedPid) {}
+  async fn set_self(&mut self, _pid: Pid) {}
 
   async fn get_actor(&self) -> Option<ActorHandle> {
     None
   }
 
   async fn get_actor_system(&self) -> ActorSystem {
-    self.system.clone()
-  }
-}
-
-#[async_trait]
-impl SenderPart for MockContext {
-  async fn get_sender(&self) -> Option<ExtendedPid> {
-    None
-  }
-
-  async fn send(&mut self, _: ExtendedPid, _: MessageHandle) {}
-
-  async fn request(&mut self, _: ExtendedPid, _: MessageHandle) {}
-
-  async fn request_with_custom_sender(&mut self, _: ExtendedPid, _: MessageHandle, _: ExtendedPid) {}
-
-  async fn request_future(&self, _: ExtendedPid, message_handle: MessageHandle, timeout: Duration) -> ActorFuture {
-    let process = ActorFutureProcess::new(self.system.clone(), timeout).await;
-    process.send_user_message(None, message_handle).await;
-    process.get_future().await
+    self.actor_system.clone()
   }
 }
 
 #[async_trait]
 impl MessagePart for MockContext {
-  async fn get_message_envelope_opt(&self) -> Option<MessageEnvelope> {
+  async fn get_message(&self) -> MessageHandle {
+    unimplemented!("Mock context does not handle messages")
+  }
+
+  async fn get_message_header_handle(&self) -> Option<ReadonlyMessageHeadersHandle> {
+    None
+  }
+
+  async fn get_message_envelope_opt(&self) -> Option<MessageOrEnvelope> {
     None
   }
 
   async fn get_message_handle_opt(&self) -> Option<MessageHandle> {
     None
   }
+}
 
-  async fn get_message_header_handle(&self) -> Option<ReadonlyMessageHeadersHandle> {
+#[async_trait]
+impl SenderPart for MockContext {
+  async fn forward(&self, _pid: &Pid) {}
+
+  async fn respond(&self, _response: ResponseHandle) {}
+
+  async fn get_sender(&self) -> Option<Pid> {
     None
   }
-}
 
-impl ReceiverContext for MockContext {}
+  async fn send(&mut self, _pid: Pid, _message_handle: MessageHandle) {}
 
-#[async_trait]
-impl ReceiverPart for MockContext {
-  async fn receive(&mut self, _: MessageEnvelope) -> Result<(), ActorError> {
-    Ok(())
-  }
-}
+  async fn request(&mut self, _pid: Pid, _message_handle: MessageHandle) {}
 
-impl SpawnerContext for MockContext {}
+  async fn request_with_custom_sender(&mut self, _pid: Pid, _message_handle: MessageHandle, _sender: Pid) {}
 
-#[async_trait]
-impl SpawnerPart for MockContext {
-  async fn spawn(&mut self, _: Props) -> ExtendedPid {
-    todo!()
-  }
-
-  async fn spawn_prefix(&mut self, _: Props, _: &str) -> ExtendedPid {
-    todo!()
-  }
-
-  async fn spawn_named(&mut self, _: Props, _: &str) -> Result<ExtendedPid, SpawnError> {
-    todo!()
+  async fn request_future(&self, _pid: Pid, _message_handle: MessageHandle) -> Result<ResponseHandle, ActorError> {
+    Err(ActorError::Unimplemented)
   }
 }
 
 #[async_trait]
-impl BasePart for MockContext {
-  fn as_any(&self) -> &dyn Any {
-    self
+impl ExtensionPart for MockContext {
+  async fn register_extension<T: 'static>(&mut self, extension: T) {
+    self.extensions.write().await.push(Box::new(extension));
   }
 
-  async fn get_receive_timeout(&self) -> Duration {
-    todo!()
+  async fn get_extension<T: 'static>(&self) -> Option<&T> {
+    for ext in self.extensions.read().await.iter() {
+      if let Some(ext) = ext.as_any().downcast_ref::<T>() {
+        return Some(ext);
+      }
+    }
+    None
   }
 
-  async fn get_children(&self) -> Vec<ExtendedPid> {
-    todo!()
-  }
-
-  async fn respond(&self, _: ResponseHandle) {
-    todo!()
-  }
-
-  async fn stash(&mut self) {
-    todo!()
-  }
-
-  async fn un_stash_all(&mut self) -> Result<(), ActorError> {
-    todo!()
-  }
-
-  async fn watch(&mut self, _: &ExtendedPid) {
-    todo!()
-  }
-
-  async fn unwatch(&mut self, _: &ExtendedPid) {
-    todo!()
-  }
-
-  async fn set_receive_timeout(&mut self, _: &Duration) {
-    todo!()
-  }
-
-  async fn cancel_receive_timeout(&mut self) {
-    todo!()
-  }
-
-  async fn forward(&self, _: &ExtendedPid) {
-    todo!()
-  }
-
-  async fn reenter_after(&self, _: ActorFuture, _: Continuer) {
-    todo!()
+  async fn get_extension_mut<T: 'static>(&mut self) -> Option<&mut T> {
+    for ext in self.extensions.write().await.iter_mut() {
+      if let Some(ext) = ext.as_any_mut().downcast_mut::<T>() {
+        return Some(ext);
+      }
+    }
+    None
   }
 }
 
 #[async_trait]
 impl StopperPart for MockContext {
-  async fn stop(&mut self, _: &ExtendedPid) {}
+  async fn stop(&self) {}
 
-  async fn stop_future_with_timeout(&mut self, _: &ExtendedPid, _: Duration) -> ActorFuture {
-    todo!()
-  }
-
-  async fn poison(&mut self, _: &ExtendedPid) {}
-
-  async fn poison_future_with_timeout(&mut self, _: &ExtendedPid, _: Duration) -> ActorFuture {
-    todo!()
-  }
+  async fn poison_pill(&self) {}
 }
 
-impl Context for MockContext {}
+impl MockContext {
+  pub fn new(actor_system: ActorSystem) -> Self {
+    Self {
+      actor_system,
+      extensions: Arc::new(RwLock::new(Vec::new())),
+    }
+  }
+}
--- core/src/actor/process.rs
+++ core/src/actor/process.rs
@@ -11,12 +11,33 @@ pub trait Process: Debug + Send + Sync + 'static {
   async fn set_dead(&self);
 }
 
+pub type ActorProcess = Box<dyn Process + Send + Sync>;
 pub type ProcessHandle = Box<dyn Process + Send + Sync>;
 
-pub fn new_process_handle<P: Process + Send + Sync + 'static>(process: P) -> ProcessHandle {
-  Box::new(process)
+impl Clone for Box<dyn Process + Send + Sync> {
+  fn clone(&self) -> Self {
+    Box::new(ClonedProcess(self.as_ref()))
+  }
 }
 
-pub fn from_box_process(process: Box<dyn Process + Send + Sync>) -> ProcessHandle {
-  process
+#[derive(Debug)]
+struct ClonedProcess<'a>(&'a (dyn Process + Send + Sync));
+
+#[async_trait]
+impl<'a> Process for ClonedProcess<'a> {
+  async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>) {
+    self.0.send_user_message(sender, message).await
+  }
+
+  async fn send_system_message(&self, message: Box<dyn Message>) {
+    self.0.send_system_message(message).await
+  }
+
+  async fn stop(&self) {
+    self.0.stop().await
+  }
+
+  async fn set_dead(&self) {
+    self.0.set_dead().await
+  }
 }
