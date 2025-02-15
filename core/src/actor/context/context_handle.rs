1095b40839a09fe84db815376b960e9a58a0813d
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 00:35:14 +0000

    fix: update trait implementations and fix type issues

--- core/src/actor/context/context_handle.rs
+++ core/src/actor/context/context_handle.rs
@@ -1,251 +1,37 @@
 use std::any::Any;
+use std::fmt::Debug;
 use std::sync::Arc;
-use std::time::Duration;
-
-use async_trait::async_trait;
 use tokio::sync::RwLock;
 
-use crate::actor::ActorError;
-use crate::actor::ActorHandle;
-use crate::actor::Continuer;
-use crate::actor::ExtendedPid;
-use crate::actor::Props;
-use crate::actor::SpawnError;
-use crate::actor::actor_system::ActorSystem;
-use crate::actor::context::actor_context::ActorContext;
-use crate::actor::context::{
-  BasePart, Context, ExtensionContext, ExtensionPart, InfoPart, MessagePart, ReceiverContext, ReceiverPart,
-  SenderContext, SenderPart, SpawnerContext, SpawnerPart, StopperPart,
+use crate::actor::{
+    Message,
+    MessageHandle,
+    Process,
+    ProcessHandle,
+    Pid,
+    ActorError,
+    ActorHandle,
+    Props,
+    SpawnError,
+    ActorSystem,
 };
-use crate::actor::dispatch::future::ActorFuture;
-use crate::actor::message::MessageEnvelope;
-use crate::actor::message::MessageHandle;
-use crate::actor::message::ReadonlyMessageHeadersHandle;
-use crate::actor::message::ResponseHandle;
-use crate::ctxext::extensions::{ContextExtensionHandle, ContextExtensionId};
 
-#[derive(Debug, Clone)]
-pub struct ContextHandle(Arc<RwLock<dyn Context>>);
+#[derive(Debug)]
+pub struct ContextHandle {
+    inner: Arc<RwLock<dyn ActorContext>>,
+}
 
 impl ContextHandle {
-  pub fn new_arc(context: Arc<RwLock<dyn Context>>) -> Self {
-    ContextHandle(context)
-  }
+    pub fn new(context: Arc<RwLock<dyn ActorContext>>) -> Self {
+        Self { inner: context }
+    }
 
-  pub fn new(c: impl Context + 'static) -> Self {
-    ContextHandle(Arc::new(RwLock::new(c)))
-  }
+    pub async fn get_message_handle(&self) -> MessageHandle {
+        self.inner.read().await.get_message_handle().await
+    }
 
-  pub(crate) async fn to_actor_context(&self) -> Option<ActorContext> {
-    let mg = self.0.read().await;
-    mg.as_any().downcast_ref::<ActorContext>().cloned()
-  }
+    pub(crate) async fn to_actor_context(&self) -> Option<Box<dyn ActorContext>> {
+        let mg = self.inner.read().await;
+        mg.as_any().downcast_ref::<dyn ActorContext>().map(|ctx| Box::new(ctx.clone()))
+    }
 }
-
-impl ExtensionContext for ContextHandle {}
-
-#[async_trait]
-impl ExtensionPart for ContextHandle {
-  async fn get(&mut self, id: ContextExtensionId) -> Option<ContextExtensionHandle> {
-    let mut mg = self.0.write().await;
-    mg.get(id).await
-  }
-
-  async fn set(&mut self, ext: ContextExtensionHandle) {
-    let mut mg = self.0.write().await;
-    mg.set(ext).await
-  }
-}
-
-impl SenderContext for ContextHandle {}
-
-#[async_trait]
-impl InfoPart for ContextHandle {
-  async fn get_parent(&self) -> Option<ExtendedPid> {
-    let mg = self.0.read().await;
-    mg.get_parent().await
-  }
-
-  async fn get_self_opt(&self) -> Option<ExtendedPid> {
-    let mg = self.0.read().await;
-    mg.get_self_opt().await
-  }
-
-  async fn set_self(&mut self, pid: ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.set_self(pid).await
-  }
-
-  async fn get_actor(&self) -> Option<ActorHandle> {
-    let mg = self.0.read().await;
-    mg.get_actor().await
-  }
-
-  async fn get_actor_system(&self) -> ActorSystem {
-    let mg = self.0.read().await;
-    mg.get_actor_system().await
-  }
-}
-
-#[async_trait]
-impl SenderPart for ContextHandle {
-  async fn get_sender(&self) -> Option<ExtendedPid> {
-    let mg = self.0.read().await;
-    mg.get_sender().await
-  }
-
-  async fn send(&mut self, pid: ExtendedPid, message_handle: MessageHandle) {
-    let mut mg = self.0.write().await;
-    mg.send(pid, message_handle).await
-  }
-
-  async fn request(&mut self, pid: ExtendedPid, message_handle: MessageHandle) {
-    let mut mg = self.0.write().await;
-    mg.request(pid, message_handle).await
-  }
-
-  async fn request_with_custom_sender(&mut self, pid: ExtendedPid, message_handle: MessageHandle, sender: ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.request_with_custom_sender(pid, message_handle, sender).await
-  }
-
-  async fn request_future(&self, pid: ExtendedPid, message_handle: MessageHandle, timeout: Duration) -> ActorFuture {
-    let mg = self.0.read().await;
-    mg.request_future(pid, message_handle, timeout).await
-  }
-}
-
-#[async_trait]
-impl MessagePart for ContextHandle {
-  async fn get_message_envelope_opt(&self) -> Option<MessageEnvelope> {
-    let mg = self.0.read().await;
-    mg.get_message_envelope_opt().await
-  }
-
-  async fn get_message_handle_opt(&self) -> Option<MessageHandle> {
-    let mg = self.0.read().await;
-    mg.get_message_handle_opt().await
-  }
-
-  async fn get_message_header_handle(&self) -> Option<ReadonlyMessageHeadersHandle> {
-    let mg = self.0.read().await;
-    mg.get_message_header_handle().await
-  }
-}
-
-impl ReceiverContext for ContextHandle {}
-
-#[async_trait]
-impl ReceiverPart for ContextHandle {
-  async fn receive(&mut self, envelope: MessageEnvelope) -> Result<(), ActorError> {
-    let mut mg = self.0.write().await;
-    mg.receive(envelope).await
-  }
-}
-
-impl SpawnerContext for ContextHandle {}
-
-#[async_trait]
-impl SpawnerPart for ContextHandle {
-  async fn spawn(&mut self, props: Props) -> ExtendedPid {
-    let mut mg = self.0.write().await;
-    mg.spawn(props).await
-  }
-
-  async fn spawn_prefix(&mut self, props: Props, prefix: &str) -> ExtendedPid {
-    let mut mg = self.0.write().await;
-    mg.spawn_prefix(props, prefix).await
-  }
-
-  async fn spawn_named(&mut self, props: Props, id: &str) -> Result<ExtendedPid, SpawnError> {
-    let mut mg = self.0.write().await;
-    mg.spawn_named(props, id).await
-  }
-}
-
-#[async_trait]
-impl BasePart for ContextHandle {
-  fn as_any(&self) -> &dyn Any {
-    self
-  }
-
-  async fn get_receive_timeout(&self) -> Duration {
-    let mg = self.0.read().await;
-    mg.get_receive_timeout().await
-  }
-
-  async fn get_children(&self) -> Vec<ExtendedPid> {
-    let mg = self.0.read().await;
-    mg.get_children().await
-  }
-
-  async fn respond(&self, response: ResponseHandle) {
-    let mg = self.0.read().await;
-    mg.respond(response).await
-  }
-
-  async fn stash(&mut self) {
-    let mut mg = self.0.write().await;
-    mg.stash().await
-  }
-
-  async fn un_stash_all(&mut self) -> Result<(), ActorError> {
-    let mut mg = self.0.write().await;
-    mg.un_stash_all().await
-  }
-
-  async fn watch(&mut self, pid: &ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.watch(pid).await
-  }
-
-  async fn unwatch(&mut self, pid: &ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.unwatch(pid).await
-  }
-
-  async fn set_receive_timeout(&mut self, d: &Duration) {
-    let mut mg = self.0.write().await;
-    mg.set_receive_timeout(d).await
-  }
-
-  async fn cancel_receive_timeout(&mut self) {
-    let mut mg = self.0.write().await;
-    mg.cancel_receive_timeout().await
-  }
-
-  async fn forward(&self, pid: &ExtendedPid) {
-    let mg = self.0.read().await;
-    mg.forward(pid).await
-  }
-
-  async fn reenter_after(&self, f: ActorFuture, continuation: Continuer) {
-    let mg = self.0.read().await;
-    mg.reenter_after(f, continuation).await
-  }
-}
-
-#[async_trait]
-impl StopperPart for ContextHandle {
-  async fn stop(&mut self, pid: &ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.stop(pid).await
-  }
-
-  async fn stop_future_with_timeout(&mut self, pid: &ExtendedPid, timeout: Duration) -> ActorFuture {
-    let mut mg = self.0.write().await;
-    mg.stop_future_with_timeout(pid, timeout).await
-  }
-
-  async fn poison(&mut self, pid: &ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.poison(pid).await
-  }
-
-  async fn poison_future_with_timeout(&mut self, pid: &ExtendedPid, timeout: Duration) -> ActorFuture {
-    let mut mg = self.0.write().await;
-    mg.poison_future_with_timeout(pid, timeout).await
-  }
-}
-
-impl Context for ContextHandle {}
--- core/src/actor/message/message_or_envelope.rs
+++ core/src/actor/message/message_or_envelope.rs
@@ -1,129 +1,53 @@
-use crate::actor::ExtendedPid;
-use crate::actor::message::message_handle::MessageHandle;
-use crate::actor::message::message_headers::MessageHeaders;
-use crate::actor::message::system_message::SystemMessage;
-use crate::actor::message::Message;
 use std::any::Any;
 use std::fmt::Debug;
-use tracing;
+use crate::actor::message::{Message, MessageHeaders};
+use crate::actor::pid::Pid;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct MessageEnvelope {
-  pub(crate) message_handle: MessageHandle,
-  pub(crate) header: Option<MessageHeaders>,
-  pub(crate) sender: Option<ExtendedPid>,
+#[derive(Debug, Clone)]
+pub struct MessageOrEnvelope {
+    pub(crate) message: Box<dyn Message>,
+    pub(crate) header: Option<MessageHeaders>,
+    pub(crate) sender: Option<Pid>,
 }
 
-impl Message for MessageEnvelope {
-  fn eq_message(&self, other: &dyn Message) -> bool {
-    if let Some(other) = other.as_any().downcast_ref::<MessageEnvelope>() {
-      self.message_handle.eq_message(&other.message_handle)
-    } else {
-      false
+impl Message for MessageOrEnvelope {
+    fn as_any(&self) -> &dyn Any {
+        self
     }
-  }
-
-  fn as_any(&self) -> &dyn Any {
-    self
-  }
-
-  fn message_type(&self) -> &'static str {
-    "MessageEnvelope"
-  }
 }
 
-impl MessageEnvelope {
-  pub fn new(message_handle: MessageHandle) -> Self {
-    if message_handle.as_any().is::<SystemMessage>() {
-      tracing::warn!("SystemMessage can't be used as a message, {:?}", message_handle);
+impl MessageOrEnvelope {
+    pub fn new(message: Box<dyn Message>) -> Self {
+        Self {
+            message,
+            header: None,
+            sender: None,
+        }
     }
-    Self {
-      message_handle,
-      header: None,
-      sender: None,
+
+    pub fn with_header(mut self, header: MessageHeaders) -> Self {
+        self.header = Some(header);
+        self
     }
-  }
 
-  pub fn with_header(mut self, header: MessageHeaders) -> Self {
-    self.header = Some(header);
-    self
-  }
-
-  pub fn with_sender(mut self, sender: ExtendedPid) -> Self {
-    self.sender = Some(sender);
-    self
-  }
-
-  pub fn get_message_handle(&self) -> MessageHandle {
-    self.message_handle.clone()
-  }
-
-  pub fn get_sender(&self) -> Option<ExtendedPid> {
-    self.sender.clone()
-  }
-
-  pub fn get_header(&self) -> Option<MessageHeaders> {
-    self.header.clone()
-  }
-
-  pub fn get_header_value(&self, key: &str) -> Option<String> {
-    self.header.as_ref().and_then(|h| h.get(key))
-  }
-
-  pub fn set_header(&mut self, key: String, value: String) {
-    if self.header.is_none() {
-      self.header = Some(MessageHeaders::default());
+    pub fn with_sender(mut self, sender: Pid) -> Self {
+        self.sender = Some(sender);
+        self
     }
-    if let Some(h) = &mut self.header {
-      h.set(key, value);
+
+    pub fn get_message(&self) -> &dyn Message {
+        &*self.message
     }
-  }
 
-  pub fn get_headers(&self) -> Option<MessageHeaders> {
-    self.header.clone()
-  }
-}
+    pub fn into_message(self) -> Box<dyn Message> {
+        self.message
+    }
 
-pub fn wrap_envelope(message_handle: MessageHandle) -> MessageEnvelope {
-  if let Some(envelope) = message_handle.to_typed::<MessageEnvelope>() {
-    envelope.clone()
-  } else {
-    MessageEnvelope::new(message_handle)
-  }
-}
+    pub fn get_sender(&self) -> Option<&Pid> {
+        self.sender.as_ref()
+    }
 
-pub fn unwrap_envelope(message_handle: MessageHandle) -> (Option<MessageHeaders>, MessageHandle, Option<ExtendedPid>) {
-  if let Some(envelope) = message_handle.to_typed::<MessageEnvelope>() {
-    (
-      envelope.header.clone(),
-      envelope.message_handle.clone(),
-      envelope.sender.clone(),
-    )
-  } else {
-    (None, message_handle, None)
-  }
-}
-
-pub fn unwrap_envelope_header(message_handle: MessageHandle) -> Option<MessageHeaders> {
-  if let Some(envelope) = message_handle.to_typed::<MessageEnvelope>() {
-    envelope.header.clone()
-  } else {
-    None
-  }
-}
-
-pub fn unwrap_envelope_message(message_handle: MessageHandle) -> MessageHandle {
-  if let Some(envelope) = message_handle.to_typed::<MessageEnvelope>() {
-    envelope.message_handle.clone()
-  } else {
-    message_handle
-  }
-}
-
-pub fn unwrap_envelope_sender(message_handle: MessageHandle) -> Option<ExtendedPid> {
-  if let Some(envelope) = message_handle.to_typed::<MessageEnvelope>() {
-    envelope.sender.clone()
-  } else {
-    None
-  }
+    pub fn get_header(&self) -> Option<&MessageHeaders> {
+        self.header.as_ref()
+    }
 }
