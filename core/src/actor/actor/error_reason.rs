5617af45ec5752b472f603af5e177977262839ed
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 00:01:25 +0000

    refactor: fix ErrorReason implementation

--- core/src/actor/actor/error_reason.rs
+++ core/src/actor/actor/error_reason.rs
@@ -3,35 +3,37 @@ use std::fmt::{Debug, Display};
 
 #[derive(Debug, Clone)]
 pub struct ErrorReason {
-  message: String,
-  cause: Option<Box<dyn Error + Send + Sync>>,
+    message: String,
+    cause: Option<Box<dyn Error + Send + Sync>>,
 }
 
 impl ErrorReason {
-  pub fn new<E: Error + Send + Sync + 'static>(error: E) -> Self {
-    Self {
-      message: error.to_string(),
-      cause: Some(Box::new(error)),
+    pub fn new<E: Error + Send + Sync + 'static>(error: E) -> Self {
+        Self {
+            message: error.to_string(),
+            cause: Some(Box::new(error)),
+        }
     }
-  }
 
-  pub fn message(&self) -> &str {
-    &self.message
-  }
+    pub fn message(&self) -> &str {
+        &self.message
+    }
 
-  pub fn cause(&self) -> Option<&(dyn Error + Send + Sync)> {
-    self.cause.as_deref()
-  }
+    pub fn cause(&self) -> Option<&(dyn Error + Send + Sync)> {
+        self.cause.as_deref()
+    }
 }
 
 impl Display for ErrorReason {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{}", self.message)
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
 }
 
 impl Error for ErrorReason {
-  fn source(&self) -> Option<&(dyn Error + 'static)> {
-    self.cause.as_deref()
-  }
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        self.cause.as_deref()
+    }
 }
+
+pub type ErrorReasonType = Box<dyn Error + Send + Sync>;
