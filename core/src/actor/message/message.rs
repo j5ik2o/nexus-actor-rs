aa76ecd3f70583832547827f6b5125da2961678a
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 01:47:59 +0000

    fix: update actor context and message trait implementations

--- core/src/actor/message/message.rs
+++ core/src/actor/message/message.rs
@@ -1,22 +1,25 @@
+//! Message trait implementation.
+
 use std::any::Any;
 use std::fmt::Debug;
 
-pub trait Message: Debug + Send + Sync + 'static {
-  fn as_any(&self) -> &dyn Any;
-  fn message_type(&self) -> &'static str {
-    std::any::type_name::<Self>()
-  }
-  fn eq_message(&self, other: &dyn Message) -> bool {
-    if let Some(other) = other.as_any().downcast_ref::<Self>() {
-      self == other
-    } else {
-      false
+pub trait Message: Debug + Send + Sync + 'static + Clone {
+    fn as_any(&self) -> &dyn Any;
+    fn message_type(&self) -> &'static str {
+        std::any::type_name::<Self>()
     }
-  }
+    fn eq_message(&self, other: &dyn Message) -> bool {
+        if let Some(other) = other.as_any().downcast_ref::<Self>() {
+            self == other
+        } else {
+            false
+        }
+    }
 }
 
-impl<T: Debug + Send + Sync + 'static + PartialEq> Message for T {
-  fn as_any(&self) -> &dyn Any {
-    self
-  }
+// Implement Message for all types that satisfy the trait bounds
+impl<T: Debug + Send + Sync + 'static + Clone + PartialEq> Message for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
 }
