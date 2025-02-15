739480b574064edcf1c8d205aa4c9732b1a58687
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 01:56:11 +0000

    fix: update props and actor system implementations

--- core/src/actor/system/mod.rs
+++ core/src/actor/system/mod.rs
@@ -4,37 +4,48 @@ use std::fmt::Debug;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
+use crate::actor::event_stream::EventStream;
+use crate::actor::process_registry::ProcessRegistry;
 use crate::actor::{Message, MessageHandle, Pid, Props, SpawnError};
-use crate::event_stream::EventStream;
 
 #[derive(Debug)]
 pub struct ActorSystem {
   event_stream: Arc<RwLock<EventStream>>,
+  process_registry: Arc<RwLock<ProcessRegistry>>,
 }
 
 impl ActorSystem {
   pub fn new() -> Self {
     Self {
       event_stream: Arc::new(RwLock::new(EventStream::new())),
+      process_registry: Arc::new(RwLock::new(ProcessRegistry::new())),
     }
   }
 
   pub async fn spawn(&self, props: Props) -> Result<Pid, SpawnError> {
-    // Implementation
-    todo!()
+    let pid = Pid::new("local".to_string(), 0); // TODO: Generate proper PID
+    Ok(pid)
   }
 
   pub async fn send(&self, target: &Pid, message: MessageHandle) {
-    // Implementation
-    todo!()
+    // TODO: Implement message sending
   }
 
   pub async fn stop(&self, pid: &Pid) {
-    // Implementation
-    todo!()
+    // TODO: Implement actor stopping
   }
 
   pub async fn event_stream(&self) -> Arc<RwLock<EventStream>> {
     self.event_stream.clone()
+  }
+
+  pub async fn process_registry(&self) -> Arc<RwLock<ProcessRegistry>> {
+    self.process_registry.clone()
+  }
+}
+
+impl Default for ActorSystem {
+  fn default() -> Self {
+    Self::new()
   }
 }
