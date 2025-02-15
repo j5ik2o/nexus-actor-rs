99689804a456cb061e4c31ba05bfb8abd17eb235
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 02:23:24 +0000

    fix: update message and actor context trait definitions

--- core/src/actor/context/actor_context.rs
+++ core/src/actor/context/actor_context.rs
@@ -12,9 +12,8 @@ use crate::actor::props::Props;
 use crate::actor::system::ActorSystem;
 use std::time::Duration;
 
-// Base trait for all context types
 #[async_trait]
-pub trait Context: Debug + Send + Sync + 'static {
+pub trait Context: Debug + Send + Sync {
   async fn get_self_opt(&self) -> Option<Pid>;
   async fn get_self(&self) -> Pid;
   async fn get_parent_opt(&self) -> Option<Pid>;
@@ -22,7 +21,6 @@ pub trait Context: Debug + Send + Sync + 'static {
   async fn get_actor_system(&self) -> Arc<RwLock<ActorSystem>>;
 }
 
-// Trait for message handling
 #[async_trait]
 pub trait MessagePart: Context {
   async fn get_message(&self) -> MessageHandle;
@@ -33,27 +31,23 @@ pub trait MessagePart: Context {
   async fn cancel_receive_timeout(&self);
 }
 
-// Trait for receiving messages
 #[async_trait]
 pub trait ReceiverPart: Context {
   async fn receive(&self, message: MessageOrEnvelope);
 }
 
-// Trait for sending messages
 #[async_trait]
 pub trait SenderPart: Context {
   async fn request(&self, target: &Pid, message: MessageHandle) -> MessageHandle;
   async fn forward(&self, target: &Pid, message: MessageHandle);
 }
 
-// Trait for spawning actors
 #[async_trait]
 pub trait SpawnerPart: Context {
   async fn spawn(&self, props: Props) -> Result<Pid, ActorError>;
   async fn spawn_prefix(&self, props: Props, prefix: &str) -> Result<Pid, ActorError>;
 }
 
-// Trait for stopping actors
 #[async_trait]
 pub trait StopperPart: Context {
   async fn watch(&self, pid: &Pid);
@@ -61,18 +55,7 @@ pub trait StopperPart: Context {
   async fn handle_failure(&self, who: Option<Pid>, error: ActorError, message: Option<MessageHandle>);
 }
 
-// Main actor context trait combining all functionality
 pub trait ActorContext: Context + MessagePart + ReceiverPart + SenderPart + SpawnerPart + StopperPart {}
-
-// Create new traits that combine specific functionality
-pub trait ReceiverContext: Context + MessagePart + ReceiverPart {}
-pub trait SenderContext: Context + SenderPart {}
-pub trait SpawnerContext: Context + SpawnerPart {}
-pub trait RootContext: Context + SenderPart + SpawnerPart + StopperPart {}
 
 // Implement blanket implementations
 impl<T> ActorContext for T where T: Context + MessagePart + ReceiverPart + SenderPart + SpawnerPart + StopperPart {}
-impl<T> ReceiverContext for T where T: Context + MessagePart + ReceiverPart {}
-impl<T> SenderContext for T where T: Context + SenderPart {}
-impl<T> SpawnerContext for T where T: Context + SpawnerPart {}
-impl<T> RootContext for T where T: Context + SenderPart + SpawnerPart + StopperPart {}
