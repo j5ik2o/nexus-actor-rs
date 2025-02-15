d8ce3739d6ece57ab3e8c5f2a0dfe932082e8390
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 00:51:02 +0000

    fix: update context and message implementations

--- core/src/actor/context/sender_context_handle.rs
+++ core/src/actor/context/sender_context_handle.rs
@@ -1,102 +1,107 @@
+use async_trait::async_trait;
+use std::any::Any;
+use std::fmt::Debug;
 use std::sync::Arc;
 use std::time::Duration;
-
-use async_trait::async_trait;
 use tokio::sync::RwLock;
 
-use crate::actor::ActorHandle;
-use crate::actor::ExtendedPid;
-use crate::actor::actor_system::ActorSystem;
-use crate::actor::context::{InfoPart, MessagePart, SenderContext, SenderPart};
-use crate::actor::dispatch::future::ActorFuture;
-use crate::actor::message::MessageEnvelope;
-use crate::actor::message::MessageHandle;
-use crate::actor::message::ReadonlyMessageHeadersHandle;
+use crate::actor::{
+  ActorContext, ActorError, ActorHandle, ActorSystem, Message, MessageHandle, MessageOrEnvelope, Pid, Props,
+  ReadonlyMessageHeadersHandle, ResponseHandle,
+};
 
-#[derive(Debug, Clone)]
-pub struct SenderContextHandle(Arc<RwLock<dyn SenderContext>>);
+#[derive(Debug)]
+pub struct SenderContextHandle(pub Arc<RwLock<dyn ActorContext>>);
 
-impl SenderContextHandle {
-  pub fn new_arc(context: Arc<RwLock<dyn SenderContext>>) -> Self {
-    SenderContextHandle(context)
-  }
-
-  pub fn new(c: impl SenderContext + 'static) -> Self {
-    SenderContextHandle(Arc::new(RwLock::new(c)))
+impl Context for SenderContextHandle {
+  fn as_any(&self) -> &dyn Any {
+    self
   }
 }
 
 #[async_trait]
 impl InfoPart for SenderContextHandle {
-  async fn get_parent(&self) -> Option<ExtendedPid> {
-    let mg = self.0.read().await;
-    mg.get_parent().await
+  async fn get_children(&self) -> Vec<Pid> {
+    self.0.read().await.get_children().await
   }
 
-  async fn get_self_opt(&self) -> Option<ExtendedPid> {
-    let mg = self.0.read().await;
-    mg.get_self_opt().await
+  async fn get_receive_timeout(&self) -> Duration {
+    self.0.read().await.get_receive_timeout().await
   }
 
-  async fn set_self(&mut self, pid: ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.set_self(pid).await
+  async fn get_parent(&self) -> Option<Pid> {
+    self.0.read().await.get_parent().await
+  }
+
+  async fn get_self_opt(&self) -> Option<Pid> {
+    self.0.read().await.get_self_opt().await
+  }
+
+  async fn set_self(&mut self, pid: Pid) {
+    self.0.write().await.set_self(pid).await
   }
 
   async fn get_actor(&self) -> Option<ActorHandle> {
-    let mg = self.0.read().await;
-    mg.get_actor().await
+    self.0.read().await.get_actor().await
   }
 
   async fn get_actor_system(&self) -> ActorSystem {
-    let mg = self.0.read().await;
-    mg.get_actor_system().await
-  }
-}
-
-#[async_trait]
-impl SenderPart for SenderContextHandle {
-  async fn get_sender(&self) -> Option<ExtendedPid> {
-    let mg = self.0.read().await;
-    mg.get_sender().await
-  }
-
-  async fn send(&mut self, pid: ExtendedPid, message_handle: MessageHandle) {
-    let mut mg = self.0.write().await;
-    mg.send(pid, message_handle).await
-  }
-
-  async fn request(&mut self, pid: ExtendedPid, message_handle: MessageHandle) {
-    let mut mg = self.0.write().await;
-    mg.request(pid, message_handle).await
-  }
-
-  async fn request_with_custom_sender(&mut self, pid: ExtendedPid, message_handle: MessageHandle, sender: ExtendedPid) {
-    let mut mg = self.0.write().await;
-    mg.request_with_custom_sender(pid, message_handle, sender).await
-  }
-
-  async fn request_future(&self, pid: ExtendedPid, message_handle: MessageHandle, timeout: Duration) -> ActorFuture {
-    let mg = self.0.read().await;
-    mg.request_future(pid, message_handle, timeout).await
+    self.0.read().await.get_actor_system().await
   }
 }
 
 #[async_trait]
 impl MessagePart for SenderContextHandle {
-  async fn get_message_envelope_opt(&self) -> Option<MessageEnvelope> {
-    let mg = self.0.read().await;
-    mg.get_message_envelope_opt().await
-  }
-
-  async fn get_message_handle_opt(&self) -> Option<MessageHandle> {
-    let mg = self.0.read().await;
-    mg.get_message_handle_opt().await
+  async fn get_message(&self) -> MessageHandle {
+    self.0.read().await.get_message().await
   }
 
   async fn get_message_header_handle(&self) -> Option<ReadonlyMessageHeadersHandle> {
-    let mg = self.0.read().await;
-    mg.get_message_header_handle().await
+    self.0.read().await.get_message_header_handle().await
+  }
+
+  async fn get_message_envelope_opt(&self) -> Option<MessageOrEnvelope> {
+    self.0.read().await.get_message_envelope_opt().await
+  }
+
+  async fn get_message_handle_opt(&self) -> Option<MessageHandle> {
+    self.0.read().await.get_message_handle_opt().await
+  }
+}
+
+#[async_trait]
+impl SenderPart for SenderContextHandle {
+  async fn forward(&self, pid: &Pid) {
+    self.0.read().await.forward(pid).await
+  }
+
+  async fn respond(&self, response: ResponseHandle) {
+    self.0.read().await.respond(response).await
+  }
+
+  async fn get_sender(&self) -> Option<Pid> {
+    self.0.read().await.get_sender().await
+  }
+
+  async fn send(&mut self, pid: Pid, message_handle: MessageHandle) {
+    self.0.write().await.send(pid, message_handle).await
+  }
+
+  async fn request(&mut self, pid: Pid, message_handle: MessageHandle) {
+    self.0.write().await.request(pid, message_handle).await
+  }
+
+  async fn request_with_custom_sender(&mut self, pid: Pid, message_handle: MessageHandle, sender: Pid) {
+    self
+      .0
+      .write()
+      .await
+      .request_with_custom_sender(pid, message_handle, sender)
+      .await
+  }
+
+  async fn request_future(&self, pid: Pid, message_handle: MessageHandle) -> Result<ResponseHandle, ActorError> {
+    self.0.read().await.request_future(pid, message_handle).await
   }
 }
 
--- core/src/actor/message/auto_respond.rs
+++ core/src/actor/message/auto_respond.rs
@@ -7,12 +7,6 @@ pub struct AutoRespond {
   pub message: Box<dyn Message>,
 }
 
-impl AutoRespond {
-  pub fn new(message: Box<dyn Message>) -> Self {
-    Self { message }
-  }
-}
-
 impl Message for AutoRespond {
   fn as_any(&self) -> &(dyn Any + Send + Sync) {
     self
