c175fee5f546d5f51b4fb4b296bd0923906e57d1
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 01:29:39 +0000

    fix: update spawner context handle implementation

--- core/src/actor/context/spawner_context_handle.rs
+++ core/src/actor/context/spawner_context_handle.rs
@@ -6,81 +6,94 @@ use std::time::Duration;
 use tokio::sync::RwLock;
 
 use crate::actor::{
-  ActorContext, ActorError, ActorHandle, ActorSystem, Message, MessageHandle, MessageOrEnvelope, Pid, Props,
-  ReadonlyMessageHeadersHandle, ResponseHandle, SpawnError,
+  ActorSystem, Context, InfoPart, Message, MessageHandle, MessageOrEnvelope, MessagePart, Pid, Props, SpawnError,
+  SpawnerContext, SpawnerPart,
 };
 
 #[derive(Debug)]
-pub struct SpawnerContextHandle(pub Arc<RwLock<dyn ActorContext>>);
+pub struct SpawnerContextHandle {
+  inner: Arc<RwLock<ActorSystem>>,
+}
 
+impl SpawnerContextHandle {
+  pub fn new(actor_system: Arc<RwLock<ActorSystem>>) -> Self {
+    Self { inner: actor_system }
+  }
+}
+
+#[async_trait]
 impl Context for SpawnerContextHandle {
   fn as_any(&self) -> &dyn Any {
     self
+  }
+
+  async fn parent(&self) -> Option<Pid> {
+    None
+  }
+
+  async fn self_pid(&self) -> Pid {
+    unimplemented!()
+  }
+
+  async fn actor_system(&self) -> Arc<RwLock<ActorSystem>> {
+    self.inner.clone()
   }
 }
 
 #[async_trait]
 impl InfoPart for SpawnerContextHandle {
-  async fn get_children(&self) -> Vec<Pid> {
-    self.0.read().await.get_children().await
-  }
-
-  async fn get_receive_timeout(&self) -> Duration {
-    self.0.read().await.get_receive_timeout().await
-  }
-
-  async fn get_parent(&self) -> Option<Pid> {
-    self.0.read().await.get_parent().await
-  }
-
   async fn get_self_opt(&self) -> Option<Pid> {
-    self.0.read().await.get_self_opt().await
+    None
   }
 
-  async fn set_self(&mut self, pid: Pid) {
-    self.0.write().await.set_self(pid).await
+  async fn get_self(&self) -> Pid {
+    unimplemented!()
   }
 
-  async fn get_actor(&self) -> Option<ActorHandle> {
-    self.0.read().await.get_actor().await
+  async fn get_parent_opt(&self) -> Option<Pid> {
+    None
   }
 
-  async fn get_actor_system(&self) -> ActorSystem {
-    self.0.read().await.get_actor_system().await
+  async fn get_parent(&self) -> Pid {
+    unimplemented!()
+  }
+
+  async fn get_actor_system(&self) -> Arc<RwLock<ActorSystem>> {
+    self.inner.clone()
   }
 }
 
 #[async_trait]
 impl MessagePart for SpawnerContextHandle {
   async fn get_message(&self) -> MessageHandle {
-    self.0.read().await.get_message().await
-  }
-
-  async fn get_message_header_handle(&self) -> Option<ReadonlyMessageHeadersHandle> {
-    self.0.read().await.get_message_header_handle().await
+    unimplemented!()
   }
 
   async fn get_message_envelope_opt(&self) -> Option<MessageOrEnvelope> {
-    self.0.read().await.get_message_envelope_opt().await
+    None
   }
 
-  async fn get_message_handle_opt(&self) -> Option<MessageHandle> {
-    self.0.read().await.get_message_handle_opt().await
+  async fn get_message_envelope(&self) -> MessageOrEnvelope {
+    unimplemented!()
   }
+
+  async fn get_receive_timeout(&self) -> Duration {
+    Duration::from_secs(0)
+  }
+
+  async fn set_receive_timeout(&self, _duration: Duration) {}
+
+  async fn cancel_receive_timeout(&self) {}
 }
 
 #[async_trait]
 impl SpawnerPart for SpawnerContextHandle {
   async fn spawn(&self, props: Props) -> Result<Pid, SpawnError> {
-    self.0.read().await.spawn(props).await
+    self.inner.read().await.spawn(props).await
   }
 
   async fn spawn_prefix(&self, props: Props, prefix: &str) -> Result<Pid, SpawnError> {
-    self.0.read().await.spawn_prefix(props, prefix).await
-  }
-
-  async fn spawn_named(&mut self, props: Props, id: &str) -> Result<Pid, SpawnError> {
-    self.0.write().await.spawn_named(props, id).await
+    self.inner.read().await.spawn_prefix(props, prefix).await
   }
 }
 
