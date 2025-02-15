481fa75a7923f4b3b1bbcf9815805c93b552297c
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 02:02:49 +0000

    fix: update mailbox trait and handle implementations

--- core/src/actor/dispatch/mailbox.rs
+++ core/src/actor/dispatch/mailbox.rs
@@ -4,7 +4,6 @@ use async_trait::async_trait;
 use std::fmt::Debug;
 
 use crate::actor::message::MessageHandle;
-use nexus_actor_utils_rs::collections::{QueueReader, QueueWriter};
 
 #[async_trait]
 pub trait Mailbox: Debug + Send + Sync {
@@ -13,5 +12,3 @@ pub trait Mailbox: Debug + Send + Sync {
   async fn receive_system_message(&self) -> Option<MessageHandle>;
   async fn receive_user_message(&self) -> Option<MessageHandle>;
 }
-
-pub type MailboxQueue = Box<dyn QueueWriter<MessageHandle> + QueueReader<MessageHandle> + Send + Sync>;
--- core/src/actor/dispatch/mailbox_handle.rs
+++ core/src/actor/dispatch/mailbox_handle.rs
@@ -1,88 +1,41 @@
-use std::sync::Arc;
+//! Mailbox handle implementation.
 
 use async_trait::async_trait;
+use std::fmt::Debug;
+use std::sync::Arc;
 use tokio::sync::RwLock;
 
-use crate::actor::dispatch::dispatcher::DispatcherHandle;
 use crate::actor::dispatch::mailbox::Mailbox;
-use crate::actor::dispatch::message_invoker::MessageInvokerHandle;
 use crate::actor::message::MessageHandle;
 
-#[derive(Debug, Clone)]
-pub struct MailboxHandle(Arc<RwLock<dyn Mailbox>>);
-
-impl PartialEq for MailboxHandle {
-  fn eq(&self, other: &Self) -> bool {
-    Arc::ptr_eq(&self.0, &other.0)
-  }
-}
-
-impl Eq for MailboxHandle {}
-
-impl std::hash::Hash for MailboxHandle {
-  fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-    (self.0.as_ref() as *const RwLock<dyn Mailbox>).hash(state);
-  }
+#[derive(Debug)]
+pub struct MailboxHandle {
+  inner: Arc<RwLock<Box<dyn Mailbox>>>,
 }
 
 impl MailboxHandle {
-  pub fn new_arc(mailbox: Arc<RwLock<dyn Mailbox>>) -> Self {
-    MailboxHandle(mailbox)
-  }
-
-  pub fn new(mailbox: impl Mailbox + 'static) -> Self {
-    MailboxHandle(Arc::new(RwLock::new(mailbox)))
+  pub fn new(mailbox: Box<dyn Mailbox>) -> Self {
+    Self {
+      inner: Arc::new(RwLock::new(mailbox)),
+    }
   }
 }
 
 #[async_trait]
 impl Mailbox for MailboxHandle {
-  async fn get_user_messages_count(&self) -> i32 {
-    let mg = self.0.read().await;
-    mg.get_user_messages_count().await
+  async fn post_system_message(&self, message: MessageHandle) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    self.inner.write().await.post_system_message(message).await
   }
 
-  async fn get_system_messages_count(&self) -> i32 {
-    let mg = self.0.read().await;
-    mg.get_system_messages_count().await
+  async fn post_user_message(&self, message: MessageHandle) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    self.inner.write().await.post_user_message(message).await
   }
 
-  async fn process_messages(&self) {
-    let mg = self.0.read().await;
-    mg.process_messages().await;
+  async fn receive_system_message(&self) -> Option<MessageHandle> {
+    self.inner.write().await.receive_system_message().await
   }
 
-  async fn post_user_message(&self, message_handle: MessageHandle) {
-    let mg = self.0.read().await;
-    mg.post_user_message(message_handle).await;
-  }
-
-  async fn post_system_message(&self, message_handle: MessageHandle) {
-    let mg = self.0.read().await;
-    mg.post_system_message(message_handle).await;
-  }
-
-  async fn register_handlers(
-    &mut self,
-    message_invoker_handle: Option<MessageInvokerHandle>,
-    dispatcher_handle: Option<DispatcherHandle>,
-  ) {
-    let mut mg = self.0.write().await;
-    mg.register_handlers(message_invoker_handle, dispatcher_handle).await;
-  }
-
-  async fn start(&self) {
-    let mg = self.0.read().await;
-    mg.start().await;
-  }
-
-  async fn user_message_count(&self) -> i32 {
-    let mg = self.0.read().await;
-    mg.user_message_count().await
-  }
-
-  async fn to_handle(&self) -> MailboxHandle {
-    let mg = self.0.read().await;
-    mg.to_handle().await
+  async fn receive_user_message(&self) -> Option<MessageHandle> {
+    self.inner.write().await.receive_user_message().await
   }
 }
