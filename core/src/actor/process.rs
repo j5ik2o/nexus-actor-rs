aad3db0494ad20eba720d55ad0eafb4d72c32b87
Junichi Kato <j5ik2o@gmail.com>
2025-02-15 00:03:07 +0000

    refactor: fix Process trait and add ProcessHandle type

--- core/src/actor/process.rs
+++ core/src/actor/process.rs
@@ -1,16 +1,15 @@
+use std::fmt::Debug;
+use async_trait::async_trait;
 use crate::actor::message::Message;
 use crate::actor::pid::Pid;
-use async_trait::async_trait;
-use std::fmt::Debug;
 
 #[async_trait]
 pub trait Process: Debug + Send + Sync + 'static {
-  async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>);
-  async fn send_system_message(&self, message: Box<dyn Message>);
-  async fn stop(&self);
-  async fn set_dead(&self);
+    async fn send_user_message(&self, sender: Option<&Pid>, message: Box<dyn Message>);
+    async fn send_system_message(&self, message: Box<dyn Message>);
+    async fn stop(&self);
+    async fn set_dead(&self);
 }
 
 pub type ActorProcess = Box<dyn Process + Send + Sync>;
-
-// Remove implementations to avoid conflicts - these should be in their respective modules
+pub type ProcessHandle = Box<dyn Process + Send + Sync>;
