095759bd8a254f923a88c1bbcdcd1259672af840
Junichi Kato <j5ik2o@gmail.com>
2025-09-23 23:09:49 +0900

    refactor: Protocol Buffersファイルをプロジェクトルートに統合
    - 各モジュール（core, cluster, remote）に分散していた.protoファイルを/proto/に集約
    - build.rsファイルを更新して新しいパス構成に対応
    - 重複していたprotoファイルを削除してメンテナンス性を向上

--- core/build.rs
+++ core/build.rs
@@ -1,13 +1,6 @@
 fn main() {
   tonic_build::configure()
     .out_dir("generated")
-    .compile_protos(
-      &[
-        "proto/google/protobuf/duration.proto",
-        "proto/google/protobuf/any.proto",
-        "proto/actor.proto",
-      ],
-      &["proto/"],
-    )
+    .compile_protos(&["../proto/actor.proto"], &["../proto"])
     .unwrap();
 }
--- remote/build.rs
+++ remote/build.rs
@@ -3,14 +3,14 @@ fn main() {
     .out_dir("generated")
     .compile_protos(
       &[
-        "proto/remote.proto",
-        "proto/cluster.proto",
-        "proto/grain.proto",
-        "proto/pubsub.proto",
-        "proto/gossip.proto",
-        "proto/examples/remote-activate/messages.proto",
+        "../proto/remote.proto",
+        "../proto/cluster.proto",
+        "../proto/grain.proto",
+        "../proto/pubsub.proto",
+        "../proto/gossip.proto",
+        "../proto/examples/remote-activate/messages.proto",
       ],
-      &["proto/", "../core/proto"],
+      &["../proto"],
     )
     .unwrap();
 }
