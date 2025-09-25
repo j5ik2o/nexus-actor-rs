d333e28e88c49049b52f36d26c88408d0c37ea47
Junichi Kato <j5ik2o@gmail.com>
2025-09-26 02:42:34 +0900

    feat: ActorHandleに型名キャッシュを追加しパフォーマンス向上
    - ActorHandle構造体にtype_name: Arc<str>フィールドを追加
    - 型名への同期アクセスメソッドtype_name()とtype_name_arc()を実装
    - ReceiverMiddlewareChainのSafetyコメントを改善

--- core/src/actor/core/actor_handle.rs
+++ core/src/actor/core/actor_handle.rs
@@ -9,11 +9,14 @@ use crate::actor::core::actor_error::ActorError;
 use crate::actor::supervisor::SupervisorStrategyHandle;
 
 #[derive(Debug, Clone)]
-pub struct ActorHandle(Arc<RwLock<dyn Actor>>);
+pub struct ActorHandle {
+  actor: Arc<RwLock<dyn Actor>>,
+  type_name: Arc<str>,
+}
 
 impl PartialEq for ActorHandle {
   fn eq(&self, other: &Self) -> bool {
-    Arc::ptr_eq(&self.0, &other.0)
+    Arc::ptr_eq(&self.actor, &other.actor)
   }
 }
 
@@ -21,39 +24,46 @@ impl Eq for ActorHandle {}
 
 impl std::hash::Hash for ActorHandle {
   fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-    (self.0.as_ref() as *const RwLock<dyn Actor>).hash(state);
+    (self.actor.as_ref() as *const RwLock<dyn Actor>).hash(state);
   }
 }
 
 impl ActorHandle {
-  pub fn new_arc(actor: Arc<RwLock<dyn Actor>>) -> Self {
-    ActorHandle(actor)
+  pub fn new_arc(actor: Arc<RwLock<dyn Actor>>, type_name: Arc<str>) -> Self {
+    ActorHandle { actor, type_name }
   }
 
   pub fn new(actor: impl Actor + 'static) -> Self {
-    ActorHandle(Arc::new(RwLock::new(actor)))
+    let type_name = Arc::<str>::from(actor.get_type_name());
+    ActorHandle {
+      actor: Arc::new(RwLock::new(actor)),
+      type_name,
+    }
   }
 
-  pub async fn type_name(&self) -> String {
-    let mg = self.0.read().await;
-    mg.get_type_name()
+  pub fn type_name(&self) -> &str {
+    self.type_name.as_ref()
+  }
+
+  pub fn type_name_arc(&self) -> Arc<str> {
+    self.type_name.clone()
   }
 }
 
 #[async_trait]
 impl Actor for ActorHandle {
   async fn handle(&mut self, c: ContextHandle) -> Result<(), ActorError> {
-    let mut mg = self.0.write().await;
+    let mut mg = self.actor.write().await;
     mg.handle(c).await
   }
 
   async fn receive(&mut self, context_handle: ContextHandle) -> Result<(), ActorError> {
-    let mut mg = self.0.write().await;
+    let mut mg = self.actor.write().await;
     mg.receive(context_handle).await
   }
 
   async fn get_supervisor_strategy(&mut self) -> Option<SupervisorStrategyHandle> {
-    let mut mg = self.0.write().await;
+    let mut mg = self.actor.write().await;
     mg.get_supervisor_strategy().await
   }
 }
