b8f03c30ebe26c7bc892d64fd849dfa4983739e2
Junichi Kato <j5ik2o@gmail.com>
2025-09-26 17:31:07 +0900

    feat: コンテキストのスナップショット機能を追加

---
+++ core/src/actor/context/receiver_snapshot.rs
@@ -0,0 +1,33 @@
+use crate::actor::context::context_snapshot::ContextSnapshot;
+use crate::actor::message::MessageEnvelope;
+
+#[derive(Debug, Clone)]
+pub struct ReceiverSnapshot {
+  pub(crate) context: ContextSnapshot,
+  pub(crate) message: MessageEnvelope,
+}
+
+impl ReceiverSnapshot {
+  pub fn new(context: ContextSnapshot, message: MessageEnvelope) -> Self {
+    Self { context, message }
+  }
+
+  pub fn context(&self) -> &ContextSnapshot {
+    &self.context
+  }
+
+  pub fn message(&self) -> &MessageEnvelope {
+    &self.message
+  }
+
+  pub fn map_message<F>(mut self, f: F) -> Self
+  where
+    F: FnOnce(MessageEnvelope) -> MessageEnvelope, {
+    self.message = f(self.message);
+    self
+  }
+
+  pub fn into_parts(self) -> (ContextSnapshot, MessageEnvelope) {
+    (self.context, self.message)
+  }
+}
