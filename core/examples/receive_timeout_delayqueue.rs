b2b046766a5508c95957e0fe7b2abd42efaa497f
Junichi Kato <j5ik2o@gmail.com>
2025-09-26 21:34:10 +0900

    example: 受信タイムアウトとDelayQueue使用例を追加

---
+++ core/examples/receive_timeout_delayqueue.rs
@@ -0,0 +1,6 @@
+//! DelayQueue を用いた receive_timeout デモ (PoC 用・未実装)
+
+fn main() {
+  // TODO: DelayQueue を使った receive_timeout ハンドリングの PoC を実装する。
+  println!("receive_timeout_delayqueue sample は未実装です");
+}
